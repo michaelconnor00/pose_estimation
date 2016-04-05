5aa01d984ecfeb73070af8c1293d7e81d1e142d9
Michael Connor <mike@sparkgeo.com>
2016-04-04 22:37:21 -0700

    Use absolute paths and write image with corners.

--- 3D_2Dpose.py
+++ 3D_2Dpose.py
@@ -12,6 +12,7 @@ Options:
     --verbose       Print debug output
 
 """
+import os
 import numpy as np
 import cv2
 from docopt import docopt
@@ -20,10 +21,12 @@ def main(arguments):
     print arguments
     image_filename = arguments['<file_name>']
 
-    img = cv2.imread(image_filename)
+    cwd = os.getcwd()
+
+    img = cv2.imread(os.path.join(cwd, image_filename))
 
     # http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_feature2d/py_shi_tomasi/py_shi_tomasi.html
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     corners = cv2.goodFeaturesToTrack(gray, 25, 0.01, 10)
     corners = np.int0(corners)
@@ -31,6 +34,8 @@ def main(arguments):
     for i in corners:
         x, y = i.ravel()
         cv2.circle(img, (x,y), 3, 255, -1)
+
+    cv2.imwrite('orig_w_corners.jpg', img)
 
     # # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_fast/py_fast.html
     # # Initiate FAST object with default values
@@ -76,8 +81,6 @@ def main(arguments):
     # cv2.line(img, (160, 3*160), (2*160, 3*160), (255, 0, 0), 3)
     # cv2.line(img, (2*160, 3*160), (2*160, 4*160), (255, 0, 0), 3)
     # cv2.line(img, (160, 4*160), (2*160, 4*160), (255, 0, 0), 3)
-
-    # cv2.imwrite('out.jpg', img)
 
 
 if __name__ == '__main__':
